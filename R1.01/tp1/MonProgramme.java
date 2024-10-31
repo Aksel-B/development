7ecfb9b727057528c6a3785c2d9c91dcd3b23a30
Aksel-B <bouri.aksel@gmail.com>
2024-10-31 14:55:16 +0100

    Create MonProgramme.java

---
+++ R1.01/tp1/MonProgramme.java
@@ -0,0 +1,9 @@
+class MonProgramme extends Program {
+    // fonction principale appelée lors de l’exécution du programme
+    void algorithm () {
+        // contient une suite d’instructions
+        String msg; //exemple de déclaration
+        msg = "Bonjour l'univers"; // exemple d’affectation
+        println(msg); // exemple d’appel de fonction
+    }
+}
