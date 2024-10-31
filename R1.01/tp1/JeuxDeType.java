9e8d9270e2c89fbcd2460f3424e3238a46ca07d1
Aksel-B <bouri.aksel@gmail.com>
2024-10-31 14:43:24 +0100

    Update JeuxDeType.java

--- R1.01/tp1/JeuxDeType.java
+++ R1.01/tp1/JeuxDeType.java
@@ -1,11 +1,27 @@
+// Déclaration de la classe JeuxDeType qui hérite de Program 
 class JeuxDeType extends Program {
+
+    // Déclaration de la méthode principale algorithm() qui exécute le programme
     void algorithm(){
+        // Initialisation de la variable 'prenom' avec le prénom "Ada"
         String prenom = "Ada";
+        
+        // Initialisation de la variable 'nom' avec le nom de famille "Lovelace"
         String nom = "Lovelace";
+        
+        // Initialisation de l'année de naissance de la personne
         int naissance = 1815;
+        
+        // Initialisation de l'année actuelle
         int annee = 2022;
+        
+        // Calcul de l'âge estimé en soustrayant l'année de naissance à l'année actuelle
         int age = annee - naissance;
-        char initiale = charAt(prenom,0);
-        println(initiale + ". " + nom + " aurait eu " + age + "ans en " + annee);
+        
+        // Récupération de la première lettre du prénom
+        char initiale = charAt(prenom, 0); // Utilisation de charAt() pour obtenir le premier caractère du prénom
+        
+        // Affichage de la phrase finale dans la console
+        println(initiale + ". " + nom + " aurait eu " + age + " ans en " + annee);
     }
 }
